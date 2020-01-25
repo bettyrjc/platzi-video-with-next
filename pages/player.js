69a1dc5f00aa1300b35645d229a5d7b83cf58789
bettyrjc <bettyjimenez3010@gmail.com>
2020-01-24 22:21:29 -0400

    añadiendo player y estilos

--- pages/player.js
+++ pages/player.js
@@ -1,41 +1,46 @@
-import React, {useEffect} from 'react';
-import {connect} from 'react-redux';
-import Router from 'next/router'
-import {getVideoSource} from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Router from "next/router";
+import { getVideoSource } from "../actions";
 
-import css from '../components/styles/components/Player.scss'
-import NotFound from './_error'
+import css from "../components/styles/components/Player.scss";
+import NotFound from "./_error";
 
-const player = props => {
+class Player extends Component {
+  static async getInitialProps({ query }) {
+    const { id, slug } = query;
+    return { id, slug };
+  }
 
-  const { id } = props.trends.id;
-  const hasPlaying = Object.keys(props.playing).length > 0;
-  useEffect(() => {
-    props.getVideoSource(id);
-  }, []);
-  return !hasPlaying ? <NotFound/> :
-  (
-    <div className="Player">
-      <video controls autoPlay>
-        <source src={props.playing.source} type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video>
-      <div className="Player-back">
-        <button type="button" onClick={() => Router.push('/')}>
-          Regresar
-        </button>
+  async componentDidMount() {
+    await this.props.getVideoSource(this.props.id);
+  }
+
+  render() {
+    return (
+      <div className={css.Player}>
+        <video controls autoPlay>
+          <source src={this.props.playing.source} type="video/mp4" />
+          Your browser does not support HTML5 video.
+        </video>
+        <div className={css.Player_back}>
+          <button type="button" onClick={() => Router.push("/")}>
+            Regresar
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
+
 const mapStateToProps = state => {
   return {
-    playing: state.playing,
+    playing: state.playing
   };
 };
 
 const mapDispatchToProps = {
-  getVideoSource,
+  getVideoSource
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(player);
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
